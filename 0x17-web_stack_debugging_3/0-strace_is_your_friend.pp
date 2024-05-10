e26e419451c0b8922d07348a2a62f5ebad0ed063
abrahamjam <abrahamjms156@gmail.com>
2024-05-10 08:18:04 -0700

    Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,25 @@
-# Fixing Apache returning a 500 error
-
-file { '/var/www/html/wp-settings.php':
-  ensure  => file,
-  content => file('/var/www/html/wp-settings.php').content.gsub('phpp', 'php'),
+# Ensure Apache service is running
+service { 'apache2':
+  ensure => 'running',
 }
 
+# Fix the issue causing 500 error by modifying Apache configuration
+file { '/etc/apache2/sites-available/000-default.conf':
+  ensure  => file,
+  content => '
+<VirtualHost *:80>
+        ServerAdmin webmaster@localhost
+        DocumentRoot /var/www/html
+        ErrorLog ${APACHE_LOG_DIR}/error.log
+        CustomLog ${APACHE_LOG_DIR}/access.log combined
+
+        # Add the following lines to fix the issue
+        <Directory /var/www/html>
+            Options Indexes FollowSymLinks
+            AllowOverride All
+            Require all granted
+        </Directory>
+</VirtualHost>
+',
+  notify  => Service['apache2'],
+}
